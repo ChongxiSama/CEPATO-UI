baf8460481cb812ce9589e76c47e67cfa49d7a2e
ChongxiSama <chongxi3555@proton.me>
2026-04-21 09:56:55 +0000

    fix: align RpStamp colors with token system

--- src/components/RpStamp/index.tsx
+++ src/components/RpStamp/index.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export const RpStamp = () => {
   return (
-    <div className="inline-flex border border-[#1A1814] p-[1px] bg-white leading-none">
-      <span className="px-1 text-[18px] font-[900] italic text-[#1A1814]">R</span>
-      <span className="bg-[#A33332] text-white px-1.5 text-[18px] font-[900] italic">P</span>
+    <div className="inline-flex border border-lt-ink p-[1px] bg-white leading-none">
+      <span className="px-1 text-[18px] font-[900] italic text-lt-ink">R</span>
+      <span className="bg-rl-red text-white px-1.5 text-[18px] font-[900] italic">P</span>
     </div>
   )
 }
